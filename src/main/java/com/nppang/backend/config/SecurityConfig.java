eef024ea2279c33cdd650188b095df1596042823
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2025-11-12 22:30:22 +0900

    url error 수정

--- src/main/java/com/nppang/backend/config/SecurityConfig.java
+++ src/main/java/com/nppang/backend/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         // Swagger UI 경로 허용
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/index.html").permitAll()
+                        .requestMatchers("/").permitAll()
 
 
 
